e96278cebc992da1ebae9b55ac432ec78aa66a4d
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2025-01-18 21:57:24 +0200

    reactions to the post added

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -1,30 +1,30 @@
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postSlice";
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
+import ReactionButtons from "./ReactionButtons";
 
 const PostsList = () => {
+  const posts = useSelector(selectAllPosts);
 
-    const posts = useSelector(selectAllPosts) ; 
+  const renderposts = posts.map((post) => (
+    <article key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content.substring(0, 100)}</p>
+      <p>
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
+      </p>
+      <ReactionButtons post={post} />
+    </article>
+  ));
 
-    const renderposts = posts.map(post => (
-        <article key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.content.substring(0,100)}</p>
-            <PostAuthor userId={post.userId}/>
-            <TimeAgo timestamp={post.date}/>
-        </article>
-    )) ; 
+  return (
+    <>
+      <h2>Posts</h2>
+      <section>{renderposts}</section>
+    </>
+  );
+};
 
-    return (
-        <>
-            <h2>Posts</h2>
-            <section>
-                {renderposts}
-            </section>
-        </>
-    ) ; 
-}
-
-export default PostsList
-
+export default PostsList;
--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -8,6 +8,14 @@ const initialState= [
         title:'Learn Redux' , 
         content: '6 chasdfasdfasdfasdfapters' ,
         date: sub(new Date(), {minutes:10}).toISOString() , 
+        reactions: {
+            thumbsUp:0, 
+            wow : 0 , 
+            heart: 0 , 
+            rocket: 0 , 
+            coffee: 0 
+        }
+
     } ,
 
     {
@@ -15,6 +23,13 @@ const initialState= [
         title:'Learn Tailwind' , 
         content: '10 chapters',
         date: sub(new Date(), {minutes:5}).toISOString() ,
+        reactions: {
+            thumbsUp:0, 
+            wow : 0 , 
+            heart: 0 , 
+            rocket: 0 , 
+            coffee: 0 
+        }
     
     } , 
 ]
@@ -39,8 +54,23 @@ export const postsSlice = createSlice({
                         content,
                         date : new Date().toISOString() , 
                         userId , 
+                        reactions: {
+                            thumbsUp:0, 
+                            wow : 0 , 
+                            heart: 0 , 
+                            rocket: 0 , 
+                            coffee: 0 
+                        }
                     }
                 }
+            }
+        } , 
+
+        reactionAdded(state,action){
+            const {postId , reaction} = action.payload ;
+            const existingPost = state.find(post => post.id === postId)
+            if(existingPost){
+                existingPost.reactions[reaction]++ ; 
             }
         }
 
@@ -51,6 +81,6 @@ export const postsSlice = createSlice({
 
 export const selectAllPosts = (state) => state.posts ;
 
-export const {postAdded} = postsSlice.actions ; 
+export const {postAdded , reactionAdded} = postsSlice.actions ; 
 
 export default postsSlice.reducer ; 
